14917e13801ab55caa7bd407351392e6780d9300
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 03:24:53 +0700

    store mongodb uri to .env file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 mongoose
-  .connect(
-    "mongodb+srv://numanarif:akunumanarif@cluster0.ecx0a.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connection Succesful"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.json("Server start");
 });
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
