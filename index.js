dab4c89f2323a7432b4578b2f52556b540e11f44
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 02:59:48 +0700

    fix port error conn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen("PORT", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
