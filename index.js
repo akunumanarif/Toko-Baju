834b61a42a743af4256fd6387f46ff676f70ff67
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 02:58:19 +0700

    conn test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen("PORT", () => {
+  console.log(`Server running on port ${PORT}`);
+});
