761fe164b6c918f88b026a323fa385d0ee31a04c
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 03:09:33 +0700

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
 
+mongoose
+  .connect(
+    "mongodb+srv://numanarif:akunumanarif@cluster0.ecx0a.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connection Succesful"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json("Server start");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
