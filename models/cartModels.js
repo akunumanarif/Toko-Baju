cc375eb10d74881c8fe77d9f5096ab9cc306e4c3
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 05:41:47 +0700

    crt cartSchema model

---
+++ models/cartModels.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
