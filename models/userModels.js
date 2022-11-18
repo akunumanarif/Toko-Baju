98c21400721e519c43eef741ca8c06adda034fd3
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 05:38:12 +0700

    crt userchema model

--- models/userModels.js
+++ models/userModels.js
@@ -1,3 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
-const userChema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
