3f5b29b2ae7ecbbb48f6121cf18cda2316f8694a
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 06:14:11 +0700

    fix importing and usermodel name issue

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,22 @@
 import express from "express";
-import userModel from "../models/userModels.js";
+import { UserModel } from "../models/userModels.js";
 
 const router = express.Router();
 
 //? REGISTER ROUTE
-router.post("/register", (req, res) => {
-  const newUser = new userModel({
+router.post("/register", async (req, res) => {
+  const newUser = new UserModel({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   });
+
+  try {
+    const createuser = await newUser.save();
+    res.status(401).json(createuser);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 export default router;
