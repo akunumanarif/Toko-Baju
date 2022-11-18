d433de2e5df407d9b3fa2fe502966a7b088f2230
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-19 05:51:26 +0700

    setup auth post methd crt newuser

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,15 @@
 import express from "express";
+import userModel from "../models/userModels.js";
 
 const router = express.Router();
+
+//? REGISTER ROUTE
+router.post("/register", (req, res) => {
+  const newUser = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+});
 
 export default router;
