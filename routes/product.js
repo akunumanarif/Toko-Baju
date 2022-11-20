7c2c5cb02cb633d70552eae80385e0ccaae29232
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-21 06:21:15 +0700

    no need product statistic route

--- routes/product.js
+++ routes/product.js
@@ -85,31 +85,4 @@ router.get("/all", async (req, res) => {
   }
 });
 
-// //? Get User Statistic
-
-// router.get("/stats", async (req, res) => {
-//   const date = new Date();
-//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//   try {
-//     const data = await UserModel.aggregate([
-//       { $match: { createdAt: { $gte: lastYear } } },
-//       {
-//         $project: {
-//           month: { $month: "$createdAt" },
-//         },
-//       },
-//       {
-//         $group: {
-//           _id: "$month",
-//           total: { $sum: 1 },
-//         },
-//       },
-//     ]);
-//     res.status(200).json(data);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
 export default router;
