372794a8c91f4ad44ec26c2c3d0f511cd6513c65
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-21 01:21:43 +0700

    delete user method

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { route } from "express/lib/router";
 import { UserModel } from "../models/userModels.js";
 import { verifyTokenAndAuth } from "../routes/verifyToken.js";
 const router = express.Router();
@@ -23,6 +24,17 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
     );
     const { password, ...others } = updatedUser._doc;
     res.status(202).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//? Delete Route
+
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    await UserModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
   } catch (error) {
     res.status(500).json(error);
   }
