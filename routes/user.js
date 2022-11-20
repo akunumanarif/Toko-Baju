be6ddb19725996d8c5db9b4577cd47a6246fd77d
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-21 05:24:53 +0700

    fix cannot get result because not await data arrive

--- routes/user.js
+++ routes/user.js
@@ -72,6 +72,29 @@ router.get("/all", verifyTokenAndAdmin, async (req, res) => {
 
 //? Get User Statistic
 
-router.get("/stats", async (req, res) => {});
+router.get("/stats", async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await UserModel.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
