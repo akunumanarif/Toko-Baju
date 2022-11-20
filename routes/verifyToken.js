d602be5204f5e588f71a4f2abcc72905ce3d9f30
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-20 23:07:55 +0700

    split token and Barier word

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,6 +4,7 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
 
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SCRT, (err, userData) => {
       if (err) res.status(401).json("You're not authenticated");
       req.user = userData;
@@ -16,7 +17,7 @@ export const verifyToken = (req, res, next) => {
 
 export const verifyTokenAndAuth = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.userData.id === req.params.id || req.userData.isAdmin) {
+    if (req.user.id === req.params.id || req.userData.isAdmin) {
       next();
     } else {
       res.status(401).json("Unauthorized!");
