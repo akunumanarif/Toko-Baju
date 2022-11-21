d32721f2b6a885cc874cfc6dac8c57ca9aeafe1f
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-21 13:24:04 +0700

    fix bug es6 module stripe apikey use

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Stripe from "stripe";
 const router = express.Router();
-const stripe = new Stripe()(process.env.STRIPE_KEY);
+const stripe = new Stripe(process.env.STRIPE_KEY);
 
 //? CREATE PAYMENT
 
