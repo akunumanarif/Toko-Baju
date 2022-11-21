8f7b12f0fd8bdd249eabf637efa26f08d5be7548
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-21 12:36:21 +0700

    stripe create payment funct

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,25 @@
 import express from "express";
 import Stripe from "stripe";
 const router = express.Router();
+const stripe = new Stripe()(process.env.STRIPE_KEY);
+
+//? CREATE PAYMENT
+
+router.post("/", async (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (err, data) => {
+      if (err) {
+        return res.status(500).json(err);
+      } else {
+        return res.status(200).json(data);
+      }
+    }
+  );
+});
 
 export default router;
