270efac1942418b325a038a12b9bf8d8a7c53dda
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-24 13:35:12 +0700

    add token header

--- client/src/reqMethod.js
+++ client/src/reqMethod.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/"
-const TOKEN = "http://localhost:5000/api/"
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2E3MjBlYThkOGI0OWJkZWUxODI3OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTEyNDc4OCwiZXhwIjoxNjY5MzgzOTg4fQ.cC-PgWHiyigNBTVLg5g0b8yIA4UQDtSjDvaixWEn_E8"
 
 export const publicReq = axios.create({
     baseURL = BASE_URL
 })
 export const userReq = axios.create({
     baseURL = BASE_URL,
-    headers = {token: "Bearer" }
+    headers = {token: `Bearer ${TOKEN}` }
 })
