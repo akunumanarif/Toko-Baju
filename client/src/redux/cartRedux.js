24ab3f39b61dc0236e6b2e8a94b66bff2d8e8670
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-26 15:05:04 +0700

    .

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -9,9 +9,9 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      state.quantity = +1;
-      state.products.push(action.payload.products);
-      state.total = +action.payload.price;
+      state.quantity += 1;
+      state.products.push(action.payload);
+      state.total += action.payload.price * action.payload.quantity;
     },
   },
 });
