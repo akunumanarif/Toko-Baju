60846eb2482694dc31aa1e943cafe5257ffc718d
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-29 12:05:59 +0700

    .

---
+++ admin/src/dummyData.js
@@ -0,0 +1,231 @@
+export const userData = [
+	{
+		name: 'Jan',
+		'Active User': 4000
+	},
+	{
+		name: 'Feb',
+		'Active User': 3000
+	},
+	{
+		name: 'Mar',
+		'Active User': 5000
+	},
+	{
+		name: 'Apr',
+		'Active User': 4000
+	},
+	{
+		name: 'May',
+		'Active User': 3000
+	},
+	{
+		name: 'Jun',
+		'Active User': 2000
+	},
+	{
+		name: 'Jul',
+		'Active User': 4000
+	},
+	{
+		name: 'Agu',
+		'Active User': 3000
+	},
+	{
+		name: 'Sep',
+		'Active User': 4000
+	},
+	{
+		name: 'Oct',
+		'Active User': 1000
+	},
+	{
+		name: 'Nov',
+		'Active User': 4000
+	},
+	{
+		name: 'Dec',
+		'Active User': 3000
+	}
+];
+
+export const productData = [
+	{
+		name: 'Jan',
+		Sales: 4000
+	},
+	{
+		name: 'Feb',
+		Sales: 3000
+	},
+	{
+		name: 'Mar',
+		Sales: 5000
+	}
+];
+
+export const userRows = [
+	{
+		id: 1,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 2,
+		username: 'Aku Dua',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 3,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 4,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 5,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 6,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 7,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 8,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 9,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	},
+	{
+		id: 10,
+		username: 'akunuman',
+		avatar: 'https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo',
+		email: 'numanganteng@gmail.com',
+		age: 35,
+		transaction: '$200.000'
+	}
+];
+
+export const productRows = [
+	{
+		id: 1,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 2,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 3,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 4,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 5,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 6,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 7,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 8,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 9,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	},
+	{
+		id: 10,
+		name: 'Apple Airpods',
+		img: 'https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+		stock: 123,
+		status: 'active',
+		price: '$120.00'
+	}
+];
