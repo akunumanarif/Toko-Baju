7fef8b249906b1c8f4a93f1985ec003c8ed43295
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-29 12:35:15 +0700

    .

---
+++ client/src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import { loginError, loginStart, loginSuccess } from "./userRedux";
+import { publicRequest } from "../requestMethods";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginError());
+  }
+};
