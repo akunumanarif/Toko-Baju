793c95e35a4e55708ff62043861a0bfac81c713f
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-02 04:59:51 +0700

    .

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,12 +1,16 @@
 import express from "express";
-import Stripe from "stripe";
 const router = express.Router();
-const stripe = new Stripe(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51M6SReAmysRSXTA4FkEfsAxAK7hVeY52lFqD5aERMzJY1bUhZF2H4fZ4J7zrrEH4yGJjNAFum30lLVhZLKwfc0Ui007Vj9F0rn"
+);
+// const stripe = new Stripe(KEY);
 
 //? CREATE PAYMENT
 
 router.post("/payment", async (req, res) => {
-  stripe.charges.create(
+  await stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
