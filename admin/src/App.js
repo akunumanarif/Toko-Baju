482e64ff1021a5a068cbd61137258762d4ff873b
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-02 05:00:18 +0700

    .

--- admin/src/App.js
+++ admin/src/App.js
@@ -14,119 +14,6 @@ import { useSelector } from "react-redux";
 
 const App = () => {
   const admin = useSelector((state) => state.user.currentUser.isAdmin);
-  // const admin = JSON.parse(
-  //   JSON.parse(localStorage.getItem("persist:root")).user
-  // ).currentUser.isAdmin;
-
-  // const nonUser = () => {
-  //   <Navigate to="/login">
-  //     <Login></Login>
-  //   </Navigate>;
-  // };
-
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <Home></Home>
-  //       </>
-  //     ),
-  //     // children: [
-  //     //   {
-  //     //     path: "home",
-  //     //     element: <Home></Home>,
-  //     //   },
-  //     //   {
-  //     //     path: "users",
-  //     //     element: <UserLists />,
-  //     //   },
-  //     //   {
-  //     //     path: "user/:userId",
-  //     //     element: <User />,
-  //     //   },
-  //     //   {
-  //     //     path: "newUser",
-  //     //     element: <NewUser />,
-  //     //   },
-  //     //   {
-  //     //     path: "products",
-  //     //     element: <ProductList />,
-  //     //   },
-  //     // ],
-  //   },
-  //   {
-  //     path: "/users",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <UserLists />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/user/:userId",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <User />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/newUser",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <NewUser />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/products",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <ProductList />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/product/:productId",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <Product />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/newProduct",
-  //     element: (
-  //       <>
-  //         <Topbar />
-  //         <Sidebar />
-  //         <NewProduct />
-  //       </>
-  //     ),
-  //   },
-  //   {
-  //     path: "/login",
-  //     element: (
-  //       <>
-  //         <Login />
-  //       </>
-  //     ),
-  //   },
-  // ]);
-  // return admin && <RouterProvider router={router}></RouterProvider>;
 
   return (
     <Router>
@@ -136,14 +23,14 @@ const App = () => {
           <Topbar />
           <Sidebar />
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />}></Route>
 
-            <Route path="/newProduct" element={<NewProduct />} />
-            <Route path="/product/:productId" element={<Product />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/newUser" element={<NewUser />} />
-            <Route path="/user/:userId" element={<User />} />
-            <Route path="/users" element={<UserLists />} />
+            <Route path="/newProduct" element={<NewProduct />}></Route>
+            <Route path="/product/:productId" element={<Product />}></Route>
+            <Route path="/products" element={<ProductList />}></Route>
+            <Route path="/newUser" element={<NewUser />}></Route>
+            <Route path="/user/:userId" element={<User />}></Route>
+            <Route path="/users" element={<UserLists />}></Route>
           </Routes>
         </>
       )}
