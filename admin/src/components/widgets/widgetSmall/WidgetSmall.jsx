ac18608f30af10900058b9b73c9b64d27afa0db6
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-01 13:04:59 +0700

    .

--- admin/src/components/widgets/widgetSmall/WidgetSmall.jsx
+++ admin/src/components/widgets/widgetSmall/WidgetSmall.jsx
@@ -1,87 +1,48 @@
-import { Visibility } from '@material-ui/icons';
-import './widgetSmall.css';
+import { Visibility } from "@material-ui/icons";
+import "./widgetSmall.css";
+import { useState, useEffect } from "react";
+import { userRequest } from "../../../requestMethods";
 
 export default function WidgetSmall() {
-	return (
-		<div className="widgetSmall">
-			<div className="widgetSmallTitle">New Join Member</div>
-			<ul className="widgetSmallList">
-				<li className="widgetSmallListItem">
-					<img
-						src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="widgetSmallImg"
-					/>
-					<div className="widgetSmallUser">
-						<span className="widgetSmallUserName">Nunu Kul</span>
-						<span className="widgetSmallUserTitle">Software Engineer</span>
-					</div>
-					<button className="widgetSmallButton">
-						<Visibility className="widgetSmallButtonIcon" />
-						Display
-					</button>
-				</li>
-				<li className="widgetSmallListItem">
-					<img
-						src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="widgetSmallImg"
-					/>
-					<div className="widgetSmallUser">
-						<span className="widgetSmallUserName">Nunu Kul</span>
-						<span className="widgetSmallUserTitle">Software Engineer</span>
-					</div>
-					<button className="widgetSmallButton">
-						<Visibility className="widgetSmallButtonIcon" />
-						Display
-					</button>
-				</li>
-				<li className="widgetSmallListItem">
-					<img
-						src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="widgetSmallImg"
-					/>
-					<div className="widgetSmallUser">
-						<span className="widgetSmallUserName">Nunu Kul</span>
-						<span className="widgetSmallUserTitle">Software Engineer</span>
-					</div>
-					<button className="widgetSmallButton">
-						<Visibility className="widgetSmallButtonIcon" />
-						Display
-					</button>
-				</li>
-				<li className="widgetSmallListItem">
-					<img
-						src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="widgetSmallImg"
-					/>
-					<div className="widgetSmallUser">
-						<span className="widgetSmallUserName">Nunu Kul</span>
-						<span className="widgetSmallUserTitle">Software Engineer</span>
-					</div>
-					<button className="widgetSmallButton">
-						<Visibility className="widgetSmallButtonIcon" />
-						Display
-					</button>
-				</li>
-				<li className="widgetSmallListItem">
-					<img
-						src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="widgetSmallImg"
-					/>
-					<div className="widgetSmallUser">
-						<span className="widgetSmallUserName">Nunu Kul</span>
-						<span className="widgetSmallUserTitle">Software Engineer</span>
-					</div>
-					<button className="widgetSmallButton">
-						<Visibility className="widgetSmallButtonIcon" />
-						Display
-					</button>
-				</li>
-			</ul>
-		</div>
-	);
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await userRequest.get("/user/all/?new=true");
+        setUser(res.data);
+      } catch (error) {
+        console.log("Login dulu");
+      }
+    };
+    getUser();
+  }, []);
+  return (
+    <div className="widgetSmall">
+      <div className="widgetSmallTitle">New Join Member</div>
+      <ul className="widgetSmallList">
+        {users.map((user) => (
+          <li className="widgetSmallListItem" key={user._id}>
+            <img
+              src={
+                user.img ||
+                "https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg"
+              }
+              alt=""
+              className="widgetSmallImg"
+            />
+            <div className="widgetSmallUser">
+              <span className="widgetSmallUserName">{user.username}</span>
+              <span className="widgetSmallUserTitle">
+                {user.isAdmin ? "Admin" : "User"}
+              </span>
+            </div>
+            <button className="widgetSmallButton">
+              <Visibility className="widgetSmallButtonIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
