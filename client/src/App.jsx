958888fd86b343c07a6f61a2b2769f8efec9e737
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-02 04:58:07 +0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,40 +4,46 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import Success from "./pages/Success";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/products/:category",
-    element: <ProductList />,
-  },
-  {
-    path: "/product/:id",
-    element: <Product></Product>,
-  },
-  {
-    path: "/cart",
-    element: <Cart></Cart>,
-  },
-  {
-    path: "/login",
-    element: <Login></Login>,
-  },
-  {
-    path: "/register",
-    element: <Register></Register>,
-  },
-  {
-    path: "/success",
-    element: <Success />,
-  },
-]);
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },
+    {
+      path: "/products/:category",
+      element: <ProductList />,
+    },
+    {
+      path: "/product/:id",
+      element: <Product></Product>,
+    },
+    {
+      path: "/cart",
+      element: <Cart></Cart>,
+    },
+    {
+      path: "/login",
+      element: user ? <Navigate to="/" /> : <Login></Login>,
+    },
+    {
+      path: "/register",
+      element: user ? <Navigate to="/" /> : <Register />,
+    },
+    {
+      path: "/success",
+      element: <Success />,
+    },
+  ]);
   return <RouterProvider router={router} />;
 
   // (
