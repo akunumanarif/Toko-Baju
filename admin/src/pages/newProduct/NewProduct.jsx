0bb53f405912051222d871ff02c2935acd9f714c
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-02 13:31:14 +0700

    .

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,31 +1,42 @@
-import './newProduct.css';
+import "./newProduct.css";
 
 export default function NewProduct() {
-	return (
-		<div className="newProduct">
-			<h1 className="addProductTitle">New Product</h1>
-			<form className="addProductForm">
-				<div className="addProductItem">
-					<label>Image</label>
-					<input type="file" id="file" />
-				</div>
-				<div className="addProductItem">
-					<label>Name</label>
-					<input type="text" placeholder="Apple Airpods" />
-				</div>
-				<div className="addProductItem">
-					<label>Stock</label>
-					<input type="text" placeholder="123" />
-				</div>
-				<div className="addProductItem">
-					<label>Active</label>
-					<select name="active" id="active">
-						<option value="yes">Yes</option>
-						<option value="no">No</option>
-					</select>
-				</div>
-				<button className="addProductButton">Create</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="newProduct">
+      <h1 className="addProductTitle">New Product</h1>
+      <form className="addProductForm">
+        <div className="addProductItem">
+          <label>Image</label>
+          <input type="file" id="file" />
+        </div>
+        <div className="addProductItem">
+          <label>Title</label>
+          <input type="text" placeholder="Apple Airpods" />
+        </div>
+        <div className="addProductItem">
+          <label>Description</label>
+          <input type="text" placeholder="Description" />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input type="text" placeholder="Price" />
+        </div>
+        <div className="addProductItem">
+          <label>Stock</label>
+          <select name="" id="">
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
+        <div className="addProductItem">
+          <label>Active</label>
+          <select name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+        <button className="addProductButton">Create</button>
+      </form>
+    </div>
+  );
 }
