152d1cc54e1bd446bcbd606fc0ffabecc36cb861
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-03 15:56:38 +0700

    commit

---
+++ admin/src/data/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+	{
+		name: 'Jan',
+		'Active Users': 4000
+	},
+	{
+		name: 'Feb',
+		'Active Users': 2000
+	},
+	{
+		name: 'Mar',
+		'Active Users': 1000
+	},
+	{
+		name: 'Apr',
+		'Active Users': 1000
+	},
+	{
+		name: 'Mei',
+		'Active Users': 2000
+	},
+	{
+		name: 'Jun',
+		'Active Users': 1000
+	},
+	{
+		name: 'Jul',
+		'Active Users': 4000
+	},
+	{
+		name: 'Aug',
+		'Active Users': 3000
+	},
+	{
+		name: 'Sep',
+		'Active Users': 3000
+	},
+	{
+		name: 'Oct',
+		'Active Users': 4000
+	},
+	{
+		name: 'Nov',
+		'Active Users': 6000
+	},
+	{
+		name: 'Des',
+		'Active Users': 8000
+	}
+];
