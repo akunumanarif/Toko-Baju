64812c484450f86823f2c135f39a72a06f190374
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-05 07:30:23 +0700

    Token Check

--- client/src/reqMethod.js
+++ client/src/reqMethod.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 
 const BASE_URL = "https://doubtful-tuxedo-slug.cyclic.app/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+//   .currentUser.accessToken;
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2E3MjBlYThkOGI0OWJkZWUxODI3OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTgwNzk0OSwiZXhwIjoxNjcwMDY3MTQ5fQ.870YiGAH07SwNZ--QHd48mOq1v4xGUyNeKcUPlkNA00";
 
-const checkToken = (token) => {
-  if (token === TOKEN) {
-    console.log("Token Exist");
-  } else {
-    console.log("Token does not exist");
-  }
-};
+// const checkToken = (token) => {
+//   if (token === TOKEN) {
+//     console.log("Token Exist");
+//   } else {
+//     console.log("Token does not exist");
+//   }
+// };
+
 export const publicReq = axios.create({
   baseURL: BASE_URL,
 });
 export const userReq = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${checkToken(TOKEN)}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
